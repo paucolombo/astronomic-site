77851f40f42ee7e4abf4eb25e2e5859c32593420
paucolombo <mpaucolombo@gmail.com>
2023-09-15 18:14:53 +0200

    improve performance adding react lazy

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
-import { useState } from 'react'
+import { lazy, Suspense } from 'react';
 import './App.css'
 import Home from './pages/Home';
-import Apod from './pages/Apod';
-import Mars from './pages/Mars';
 import { Routes, Route } from 'react-router-dom';
+
+const Apod = lazy(() => import('./pages/Apod'));
+const Mars = lazy(() => import('./pages/Mars'));
 
 function App() {
   return (
     <>
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/apod' element={<Apod />} />
-        <Route path='/mars' element={<Mars />} />
+        <Route path='/apod' element=
+          {
+            <Suspense fallback={<div className='loading'>Loading...</div>}>
+              <Apod />
+            </Suspense>
+          }
+        />
+        <Route path='/mars' element={
+          <Suspense fallback={<div className='loading'>Loading...</div>}>
+            <Mars />
+          </Suspense>} />
       </Routes>
     </>
   )
